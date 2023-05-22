71927016e543fbb9487c22f8f82d39a8de94f688
Jason Wilson <protahjay@gmail.com>
2023-05-22 18:28:52 -0400

    added admin panel and dynamic scanner functionality

--- index.js
+++ index.js
@@ -1,61 +1,93 @@
-const SerialPort = require('serialport');
+const net = require('net');
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const path = require('path');
+const { Builder } = require('xmlbuilder');
+const moment = require('moment');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const scanners = []; // Empty array to store scanner information
+const scannersFilePath = path.join(__dirname, 'scanners.json');
 
-// Connect to the serial over ethernet servers
-const serialPorts = scanners.map((scanner) => new SerialPort(scanner));
+let scanners = [];
 
-// Listen for incoming barcode scans
-serialPorts.forEach((port) => {
-  port.on('data', (data) => {
-    const date = new Date();
-    const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const day = date.getDate();
-    const folderPath = path.join(__dirname, `${year}-${month}-${day}`);
-    if (!fs.existsSync(folderPath)) {
-      fs.mkdirSync(folderPath);
-    }
-    const fileName = path.join(folderPath, `${port.path}.txt`);
-    fs.appendFileSync(fileName, data.toString());
+// Create a socket server
+const server = net.createServer((socket) => {
+  socket.on('data', (data) => {
+    const barcodeData = data.toString().trim();
+
+    // Process and save the barcode data to XML files
+    const date = moment().format('YYYY-MM-DD');
+    const fileName = `${date}.xml`;
+    const filePath = path.join(__dirname, fileName);
+
+    const xmlRoot = Builder.create('BarcodeData');
+    const barcodeElement = xmlRoot.ele('Barcode');
+    barcodeElement.txt(barcodeData);
+
+    const xmlString = xmlRoot.end({ pretty: true });
+
+    fs.appendFileSync(filePath, xmlString);
   });
 });
-
-const configDir = path.resolve(__dirname, 'config');
 
 // Set up the views directory and view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// Create routes for the web UI
-app.get('/config', (req, res) => {
-  const configFilePath = path.join(configDir, 'config.json');
-  const configData = fs.readFileSync(configFilePath, 'utf8');
-  res.send(configData);
-});
+// Load scanners from file on server start
+function loadScannersFromFile() {
+  if (fs.existsSync(scannersFilePath)) {
+    const scannersData = fs.readFileSync(scannersFilePath, 'utf8');
+    scanners = JSON.parse(scannersData);
+  }
+}
 
-app.post('/config', (req, res) => {
-  const configFilePath = path.join(configDir, 'config.json');
-  const configData = JSON.stringify(req.body);
-  fs.writeFileSync(configFilePath, configData);
-  res.send('Config updated');
+// Save scanners to file
+function saveScannersToFile() {
+  const scannersData = JSON.stringify(scanners);
+  fs.writeFileSync(scannersFilePath, scannersData, 'utf8');
+}
+
+// Default route for the root path
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
 });
 
 // Route to render the admin control panel
 app.get('/admin', (req, res) => {
-  res.render('admin/admin');
+  res.render('admin/admin', { scanners });
 });
 
-// Start the server
+// Create routes for the web UI
+app.get('/config', (req, res) => {
+  res.json(scanners);
+});
+
+app.post('/config', (req, res) => {
+  const { name, host, port } = req.body;
+  const scanner = {
+    name,
+    host,
+    port: parseInt(port),
+  };
+  scanners.push(scanner);
+  saveScannersToFile(); // Save scanners to file
+  res.redirect('/admin'); // Redirect to refresh the admin panel
+});
+
+// Start the server and socket
 const port = 3000;
+const socketPort = 9898; // Socket port for serial I/O
+
+server.listen(socketPort, () => {
+  console.log(`Socket server listening on port ${socketPort}`);
+  loadScannersFromFile(); // Load scanners from file on server start
+});
+
 app.listen(port, () => {
-  console.log('Server listening on port 3000');
+  console.log(`Server listening on port ${port}`);
 });
